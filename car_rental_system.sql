4c4f35b261a86a63ac18d586b391d1dadfa9dc40
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-03-24 22:10:43 +0800

    Updated Database ( Removed Column [Age] )

--- car_rental_system.sql
+++ car_rental_system.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 24, 2025 at 09:51 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.1.25
+-- Generation Time: Mar 24, 2025 at 03:09 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,6 @@ SET time_zone = "+00:00";
 CREATE TABLE `users` (
   `UserID` int(11) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Age` int(11) NOT NULL,
   `PhoneNumber` varchar(11) NOT NULL,
   `Email` varchar(50) NOT NULL,
   `DoB` date NOT NULL,
@@ -39,6 +38,13 @@ CREATE TABLE `users` (
   `Password` varchar(100) NOT NULL,
   `DateCreated` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`UserID`, `Name`, `PhoneNumber`, `Email`, `DoB`, `DriversLicense`, `Role`, `Password`, `DateCreated`) VALUES
+(1, 'Lex Code', '09634356322', 'lexcode@gmail.com', '2003-10-16', '125-98-210832', 'Customer', '$2y$10$M93Kq6TIVLZC75yRCa17NeoYSDzjScTLg.EH1tciUj3UEDTxHJGgu', '2025-03-24 21:48:11');
 
 --
 -- Indexes for dumped tables
@@ -50,7 +56,8 @@ CREATE TABLE `users` (
 ALTER TABLE `users`
   ADD PRIMARY KEY (`UserID`),
   ADD UNIQUE KEY `DLicense` (`DriversLicense`),
-  ADD UNIQUE KEY `PhoneNo` (`PhoneNumber`);
+  ADD UNIQUE KEY `PhoneNo` (`PhoneNumber`),
+  ADD UNIQUE KEY `Email` (`Email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -60,7 +67,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
