1100972598c23388c45c6ccb7e2727f3e87537a9
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-03-27 10:08:48 +0800

    New Functions Added

--- home/index.php
+++ home/index.php
@@ -22,16 +22,24 @@
       echo "<button>Dashboard</button><br>
             <button>Brands</button><br>
             <button>Vehicles</button><br>
+            <button>Locations</button><br>
             <button>Bookings</button><br>
             <button>Users</button><br>
             <button>Tickets</button>";
     }elseif($_SESSION["role"] == "Customer"){
-      $carQuery = "SELECT * FROM cars";
+      $carQuery = "SELECT brands.BrandName AS Brand, car.Model, car.FuelType, car.Transmission, car.RentalPrice, (SELECT locations.Address FROM locations WHERE car.locationID = locations.LocationID) AS Location, car.Availability FROM cars AS car INNER JOIN brands ON car.BrandID = brands.BrandID";
       
       $execQuery = mysqli_query($conn, $carQuery);
       while($car = mysqli_fetch_assoc($execQuery)){
-        echo "Brand: " . $car["BrandID"] . "<br>";
+        echo "Brand: " . $car["Brand"] . "<br>";
         echo "Model: " . $car["Model"] . "<Br>";
+        echo "Fuel Type: " . $car["FuelType"] . "<Br>";
+        echo "Transmission: " . $car["Transmission"] . "<Br>";
+        echo "Price/Day: " . $car["RentalPrice"] . "<Br>";
+        echo "Location: " . $car["Location"] . "<Br>";
+        echo "Is Available: ";
+        echo $car["Availability"] == 0 ? "False" : "True";
+        echo "<br><br>";
       }
     }else{
       echo "No Account Detected!";
