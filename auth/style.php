e6497083cdde6405828115e4f6423828dd09a24c
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-03-28 22:30:44 +0800

    Finished Styling Signup Page

--- auth/style.php
+++ auth/style.php
@@ -19,23 +19,50 @@
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        background-image: url("../home/images/backgrounds/signupBG.jpg");
+        background-size: cover;
     }
     
+    body > h2 {
+        color: #FDFFF6;
+        font-size: 32px;
+        margin-bottom: 5px;
+    }
+    
+    body > p {
+        color: #FDFFF6;
+        font-size: 14px;
+        margin-bottom: 25px;
+    }
+    
+    body > h2, body > p {
+        position: relative;
+        bottom: 20px;
+    }
+    /*031A09
+    316C40
+    294E28
+    E2F87B
+    FDFFF6*/
     form {
+        position: relative;
+        bottom: 20px;
         width: 325px;
-        height: 283px;
-        border: 2px solid black;
+        height: 340px;
+        outline: 1px solid #294E28;
         border-radius: 12px;
         padding-block: 30px;
         padding-inline: 25px;
         /*transition: 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);*/
+        backdrop-filter: blur(5px);
+        color: #FDFFF6;
         transition: all 0.2s ease;
         overflow: hidden;
     }
     
     .stepsWrapper {
         position: relative;
-        height: 190px;
+        height: 219px;
         right: 0px;
         display: flex;
         flex-direction: row;
@@ -47,18 +74,27 @@
         position: relative;
         transform: translate(7px, -30px);
         height: 19px;
+        color: #FDFFF690;
         pointer-events: none;
         transition: all 0.2s;
     }
     
     input {
         outline: none;
-        border: 1px solid black;
+        border: 1px solid #00000000;
         border-radius: 6px;
         padding-top: 16px;
         padding-bottom: 4px;
         padding-inline: 5px;
         font-size: 16px;
+        background-color: #FDFFF615;
+        color: #FDFFF6;
+    }
+    
+    input:-webkit-autofill {
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: #ffffff;
+        box-shadow: inset 0 0 20px 20px #FDFFF615;
     }
     
     input:not(#firstname, #lastname, #doB):focus + label, input:not(:placeholder-shown, #dob, #firstname, #lastname) + label, #firstname:focus ~ label:nth-child(3), #firstname:not(:placeholder-shown) ~ label:nth-child(3), #lastname:focus ~ label:nth-child(4), #lastname:not(:placeholder-shown) ~ label:nth-child(4){
@@ -67,13 +103,12 @@
     }
     
     input:not(:placeholder-shown, .dob), .dobNotEmpty{
-        border-bottom: 2px solid green;
-        border-right: 2px solid green;
+        border-bottom: 2px solid #031A09;
+        border-right: 2px solid #031A09;
     }
     
     input:focus{
-        border-bottom: 2px solid purple;
-        border-right: 2px solid purple;
+        border: 1px solid #FDFFF6;
     }
     
     .fullName {
@@ -84,27 +119,26 @@
     }
     
     .fullName > input {
-        width: 134px;
+        width: 135px;
     }
     
     .fullName > input:nth-child(2), .fullName > label:nth-child(4){
-        margin-left: 2.5px;
+        margin-left: 4.5px;
     }
     
     #doB {
-        background-color: transparent;
+        background-color: #FDFFF615;
         padding-inline: 0px;
         padding-bottom: 2px;
-    }
-    
-    #doB:focus{
-        border-bottom: 1px solid purple;
-        border-right: 1px solid purple;  
     }
     
     .dobNotEmpty + label {
         font-size: 12px;
         transform: translate(5px, -38px);
+    }
+    
+    #cPass {
+        margin-top: 10px;
     }
     
     .firstStep, .secondStep, .lastStep {
@@ -114,9 +148,19 @@
         animation: slideLeft 1s;
     }
     
+    .firstStep > h4, .secondStep > h4, .lastStep > h4 {
+        font-size: 18px;
+        margin-bottom: 15px;
+    }
+    
+    .secondStep, .lastStep {
+        visibility: hidden;
+        transition: all 1s;
+    }
+    
     .lastStep > input:not(:nth-last-child(2)), .lastStep > label:not(:last-child) {
-        color: #00000080;
-        border-color: #00000080;
+        color: #FDFFF680;
+        border-color: #FDFFF680;
     }
     
     .navigation {
@@ -126,20 +170,20 @@
     }
     
     .nextButton, .previousButton {
-        border: 1px solid black;
+        border: 1px solid #FDFFF6;
         border-radius: 6px;
         padding-block: 6px;
-        width: 35%;
-        background-color: black;
+        width: 34%;
+        background-color: #FDFFF615;
         color: white;
         font-weight: 500;
         text-align: center;
     }
     
     .previousButton {
-        border: 1px solid black;
-        background-color: white;
-        color: black;
+        border: none;
+        background-color: #FDFFF615;
+        color: #FDFFF6;
         font-weight: normal;
         opacity: 0;
         transition: all 0.2s ease;
@@ -147,40 +191,52 @@
     
     .loginButton {
         position: relative;
-        top: 10px;
+        top: 20px;
         display: flex;
         flex-direction: row;
     }
     
     .loginButton > a, .loginButton > a:visited, .loginButton > p {
-        color: black;
+        color: #FDFFF6;
         font-size: 14px;
+        position: relative;
+        bottom: 20px;
     }
     
-    .show {
-        display: flex;
-        animation: slideRight 1s;
-    }
-
-    @keyframes slideRight {
-        0%{
-            opacity: 0;
-            transform: translateX(30px);
-        }
-        100%{
-            opacity: 1;
-            transform: translateX(0px);
-        }
+    .progress {
+        position: relative;
+        left: 10px;
+        height: 5px;
+        width: 255px;
+        border: 1px solid #FDFFF615;
+        overflow: hidden;
     }
     
-    @keyframes slideLeft {
-        0%{
-            opacity: 0;
-            transform: translateX(-30px);
+    .progressBar {
+        display: block;
+        width: 33.3%;
+        height: 5px;
+        background-color: #00CC0050;
+        animation: loadProgress 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+        transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+    
+    #errorMsg {
+        color: #E00;
+        width: 275px;
+        height: 1rem;
+        margin-top: 5px;
+        margin-bottom: 5px;
+        font-size: 12px;
+        text-align: center;
+    }
+    
+    @keyframes loadProgress {
+        from{
+            width: 0%;
         }
-        100%{
-            opacity: 1;
-            transform: translateX(0px);
+        to{
+            width: 33.3%;
         }
     }
 </style>
