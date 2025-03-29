2804909eedc00fc7405a3fa944c0700675bc411e
AylexCODE <AlexanderlMC@gmail.com>
2025-03-30 01:12:19 +0800

    Innitial Design

--- home/index.php
+++ home/index.php
@@ -22,33 +22,76 @@
 <?php
     if(isset($_SESSION["email"])){
         if($_SESSION["role"] == "Customer"){
+            echo "<span class='navIndicator'></span>";
             echo "<nav>
                     <span>
                         <h3>Quick Ride</h3>
-                        <p>09218912891</p>
-                        <button>Home</button>
-                        <button>About</button>
-                        <button>Contact</button>
+                        <button onclick='setActiveBtn(1)' id='homeBtn' class='active'>Home</button>
+                        <button onclick='setActiveBtn(2)' id='aboutBtn'>About</button>
+                        <button onclick='setActiveBtn(3)' id='contactBtn'>Contact</button>
                     </span>
                     <span>
                         <a href='../auth/logout.php'>logout</a>
-                    </span>";
+                    </span>
+                </nav>";
         }
     }else{
+        echo "<span class='navIndicator'></span>";
         echo "<nav>
                 <span>
                     <h3>Quick Ride</h3>
-                    <button>Home</button>
-                    <button>About</button>
-                    <button>Contact</button>
+                    <button onclick='setActiveBtn(1)' id='homeBtn' class='active'>Home</button>
+                    <button onclick='setActiveBtn(2)' id='aboutBtn'>About</button>
+                    <button onclick='setActiveBtn(3)' id='contactBtn'>Contact</button>
                 </span>
-                <span>
-                    <a href='../auth/login.php'>Sign In</a>
+                <span class='authGuest'>
+                    <a href='../auth/login.php'>Log In</a>
                     <a href='../auth/signup.php'>Sign Up</a>
                 </span>
             </nav>";
+        echo "<div class='homeWrapper'>
+                <div class='guestBG'>
+                    <span>
+                        <p>Fast & Affordable</p>
+                    </span>
+                </div>";
+        echo "<section class='carsWrapper'>
+                <span class='carFilter'>
+                    <span>
+                        <p>Filter by</p>
+                        <input list='transmissionFilter' onfocus='this.value = &#x27;&#x27;' onchange='this.blur();' placeholder='Transmission'>
+                        <datalist id='transmissionFilter'>
+                            <option>Manual</option>
+                            <option>Auto</option>
+                        </datalist>
+                        <input list='brandsFilter' onfocus='this.value = &#x27;&#x27;' onchange='this.blur();' placeholder='Brands'>
+                        <datalist id='brandsFilter'>
+                            <option>Toyota</option>
+                            <option>Ferrari</option>
+                        </datalist>
+                        <input list='fuelTypeFilter' onfocus='this.value = &#x27;&#x27;' onchange='this.blur();' placeholder='Fuel Type'>
+                        <datalist id='fuelTypeFilter'>
+                            <option>Petrolium</option>
+                            <option>Diesel</option>
+                        </datalist>
+                        <input list='modelFilter' onfocus='this.value = &#x27;&#x27;' onchange='this.blur();' placeholder='Model'>
+                        <datalist id='modelFilter'>
+                            <option>GTR 40</option>
+                            <option>Ford</option>
+                        </datalist>
+                        <button>Clear All Filter</button>
+                    </span>
+                    <span>
+                        <p>Sort by</p>
+                        <select>
+                            <option>Alphabet</option>
+                            <option>Newest</option>
+                            <option>Oldest</option>
+                    </span>
+                </span>
+            </section>";
 
-        echo "<div class='guestBG'></div>";
+        echo "</div>";
     }
 
     if(isset($_SESSION["email"])){
@@ -62,4 +105,46 @@
     }
 ?>
 </body>
+<script type="text/javascript">
+    const homeBtn = document.getElementById("homeBtn");
+    const aboutBtn = document.getElementById("aboutBtn");
+    const contactBtn = document.getElementById("contactBtn");
+    const navIndicator = document.querySelector(".navIndicator");
+    const guestBg = document.querySelector(".guestBG");
+    let activeNav = 1;
+
+    function setActiveBtn(index){
+        homeBtn.classList.remove("active");
+        aboutBtn.classList.remove("active");
+        contactBtn.classList.remove("active");
+
+        activeNav = index;
+        switch(index){
+            case 1:
+                homeBtn.classList.add("active");
+                navIndicator.style.left = homeBtn.offsetLeft-4 +"px";
+                navIndicator.style.width = homeBtn.clientWidth+8 +"px";
+                break;
+            case 2:
+                aboutBtn.classList.add("active");
+                navIndicator.style.left = aboutBtn.offsetLeft-4 +"px";
+                navIndicator.style.width = aboutBtn.clientWidth+8 +"px";
+                break;
+            case 3:
+                contactBtn.classList.add("active");
+                navIndicator.style.left = contactBtn.offsetLeft-4 +"px";
+                navIndicator.style.width = contactBtn.clientWidth+8 +"px";
+                break;
+        }
+    }
+
+    window.onload = () => {
+        navIndicator.style.left = homeBtn.offsetLeft-4 +"px";
+        navIndicator.style.width = homeBtn.clientWidth+8 +"px";
+    }
+
+    window.onresize = () => {
+        setActiveBtn(activeNav);
+    }
+</script>
 </html>
--- home/style.php
+++ home/style.php
@@ -13,13 +13,16 @@
         user-select: none;
     }
 
+    ::-webkit-scrollbar {
+        display: none;
+    }
+
     body{
         height: 100dvh;
         width: 100dvw;
         background-color: #294E28;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+        overflow-y: scroll;
+        scroll-behavior: smooth;
     }
 
     nav{
@@ -36,9 +39,154 @@
         z-index: 99;
     }
 
+    nav > span {
+        display: flex;
+        flex-direction: row;
+        text-wrap: nowrap;
+        margin-inline: 2%;
+        gap: 5%;
+        width: 20%;
+    }
+
+    nav > span:nth-child(1) {
+        gap: 20%;
+    }
+
+    nav > span:nth-child(1) > button {
+        transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+
+    nav > span:nth-child(1) > .active {
+        transform: translateY(-2px);
+        transition: all 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+    }
+
+    .navIndicator {
+        position: fixed;
+        top: 30px;
+        height: 2px;
+        background-color: #E2F87B;
+        z-index: 100;
+        transition: all 0.5s cubic-bezier(0.19, 1, 0.22, 1);
+    }
+
+    .authGuest {
+        gap: 3%;
+        justify-content: right;
+    }
+
+    .authGuest > a {
+        border: 1px solid #FDFFF6;
+        background-color: #FDFFF620;
+        border-radius: 8px;
+        padding-block: 3px;
+        padding-inline: 13px;
+        font-weight: 500;
+    }
+    
+    .authGuest > a:nth-child(1) {
+        background-color: #FDFFF6;
+        color: #091A09;
+        font-weight: 700;
+    }
+
+    nav > span > h3, nav > span > p, nav > span > button, nav > span > a, nav > span > a:visited {
+        color: #FDFFF6;
+        text-decoration: none;
+        background-color: transparent;
+        border: none;
+    }
+
     .guestBG {
         width: 100%;
-        height: 100px;
+        height: 65%;
+        border-radius: 15px;
+        overflow: hidden;
+        position: sticky;
+        top: -40%;
+    }
+
+    .guestBG > span {
+        width: 100dvw;
+        height: 100dvh;
+        transform: translateY(20%);
+        display: block;
         background-image: url("./images/backgrounds/homeBG.jpg");
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: top;
+        animation: guestBGScroll linear;
+        animation-timeline: scroll(root);
+        animation-range-start: contain;
+        animation-range-end: 250px;
+    }
+
+    .guestBG > span > p {
+        color: #FDFFF6;
+        text-transform: uppercase;
+        font-size: 32px;
+        font-weight: 700;
+        text-align: center;
+        height: 65%;
+        transform: translateY(50%);
+        text-shadow: 2px 2px #091A09;
+    }
+
+    @keyframes guestBGScroll {
+        from {
+            transform: translateY(0px);
+        }
+        to {
+            transform: translateY(20%);
+        }
+    }
+
+    .carsWrapper {
+        height: 75%;
+        width: 100%;
+        margin-top: 5px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        color: #FDFFF6;
+    }
+
+    .carFilter {
+        width: 85%;
+        margin-top: 5px;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .carFilter > span > p {
+        font-size: 12px;
+        opacity: .8;
+        margin-bottom: 3px;
+    }
+
+    .carFilter > span > input, .carFilter > span > button, .carFilter > span > select {
+        border: 2px solid #091A0970;
+        background-color: #316C40;
+        color: #FDFFF6;
+        text-align: center;
+        width: 150px;
+        padding-left: 15px;
+        padding-block: 5px;
+        border-radius: 5px;
+    }
+
+    .carFilter > span > button, .carFilter > span > select {
+        border: none;
+        padding-left: unset;
+    }
+
+    .carFilter > span > select {
+        width: 130px;
+    }
+
+    .carFilter > span > input::placeholder {
+        color: #FDFFF6;
     }
 </style>
