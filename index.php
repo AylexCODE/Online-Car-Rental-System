733409648e9a9d62c92b222f663ecfbbb7cd9a87
AylexCODE <130986014+AylexCODE@users.noreply.github.com>
2025-05-16 23:20:55 +0800

    Added Loading Screen

--- index.php
+++ index.php
@@ -6,6 +6,37 @@
             margin: 0;
             padding: 0;
         }
+
+        .loader {
+            height: 20px;
+            width: 20px;
+            border-radius: 50%;
+            background-color: #FFF;
+            animation: 1.5s pulse infinite ease-in-out;
+        }
+        
+        @keyframes pulse {
+            0% {
+            box-shadow: 0 0 0 0 #FFF;
+            }
+        
+            100% {
+            box-shadow: 0 0 0 14px #00000000;
+            }
+        }
+
+        .loaderWrapper {
+            position: fixed;
+            top: 0;
+            left: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100dvh;
+            width: 100dvw;
+            z-index: 999;
+            background-color: #316C40;
+        }
     </style>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +45,8 @@
     <title>Quick Ride</title>
 </head>
 <body>
-    <h4>Testing Preeeeee.....</h4>
+    <div class="loaderWrapper">
+        <div class="loader"></div>
+    </div>
 </body>
 </html>
